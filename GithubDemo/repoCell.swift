158109231fc033fa9287bae12dd439b79a5a67b8
GRIFFINC3 <changrif@mac.com>
2017-01-29 14:46:19 -0500

    first commit

---
+++ GithubDemo/repoCell.swift
@@ -0,0 +1,46 @@
+//
+//  repoCell.swift
+//  GithubDemo
+//
+//  Created by Chandler Griffin on 1/27/17.
+//  Copyright © 2017 codepath. All rights reserved.
+//
+
+import UIKit
+
+class repoCell: UITableViewCell {
+
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var avatarImageView: UIImageView!
+    @IBOutlet weak var ownerLabel: UILabel!
+    @IBOutlet weak var descriptionLabel: UILabel!
+    @IBOutlet weak var starImageView: UIImageView!
+    @IBOutlet weak var branchImageView: UIImageView!
+    @IBOutlet weak var branchLabel: UILabel!
+    @IBOutlet weak var starLabel: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+        branchImageView.image = #imageLiteral(resourceName: "fork")
+        starImageView.image = #imageLiteral(resourceName: "star")
+        nameLabel.preferredMaxLayoutWidth = nameLabel.frame.size.width
+    }
+    
+    override func layoutIfNeeded() {
+        super.layoutIfNeeded()
+        nameLabel.preferredMaxLayoutWidth = nameLabel.frame.size.width
+    }
+    
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        nameLabel.preferredMaxLayoutWidth = nameLabel.frame.size.width
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
